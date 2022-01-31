d8354ca2850b1c537ca8e02de89cc99d05f7963f
shaneknopp <sknopp@colgate.edu>
2022-01-31 12:04:03 -0500

    Completed Question 2 (vector calculations)

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,37 @@
+# test print statement
 print("Hello World")
+
+# simple test calculations
+2^10
+
+365+180
+
+22-44
+
+100+(-1)
+
+100+-1
+
+# variable calculation
+variable <- 10000
+
+variable/10
+
+# vector test calculations
+vector1 <- c(100000, 10000, 1000, 100)
+# divide all numbers by 10
+vector1/10
+
+vector2 <- c(1, 2, 4, 8)
+# multiply all numbers by 2
+vector2*2
+
+# divide all numbers in vector1 by all numbers in vector2
+vector3 <- vector1/vector2
+# display vector3
+vector3
+
+# creating a 100 element vector using : notation
+vector4 <- 1:100
+# divide all numbers by 2
+vector4/2

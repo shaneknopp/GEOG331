6f730c38df7fdc8b39fbe4976f247b16a082e33c
shaneknopp <sknopp@colgate.edu>
2022-02-08 12:49:04 -0500

    Dynamic file path for OS

--- Activity2.R
+++ Activity2.R
@@ -1,3 +1,15 @@
 # GEOG 331 Activity 2
 # SK 02/08/22
 
+OS <- .Platform$OS.type
+
+if (OS == "unix"){
+  temp_path <- "/Volumes/class/GEOG331_S22/students/sknopp/data/noaa_weather/" # MAC file path
+} else if (OS == "windows"){
+  temp_path <- "Z:/GEOG331_S22/students/sknopp/data/noaa_weather" # windows file path
+} else {
+  print("ERROR: OS could not be identified")
+}
+setwd(temp_path)
+datW <- read.csv("2011124.csv")
+

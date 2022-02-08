e250f57c12b4f11e28135b104f9c7fa85e5dbecc
shaneknopp <sknopp@colgate.edu>
2022-02-08 14:34:13 -0500

    Question 1

--- Activity2.R
+++ Activity2.R
@@ -11,5 +11,23 @@ if (OS == "unix"){
   print("ERROR: OS could not be identified")
 }
 setwd(temp_path)
-datW <- read.csv("2011124.csv")
 
+#Question 1
+datW <- read.csv("2011124.csv")
+nrow(datW)
+ncol(datW)
+
+#specify a column with a proper date format
+#note the format here dataframe$column
+datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
+#google date formatting in r to find more options and learn more
+
+#create a date column by reformatting the date to only include years
+#and indicating that it should be treated as numeric data
+datW$year <- as.numeric(format(datW$dateF,"%Y"))
+
+#Question 2
+charvec <- c("One", "Two", "Three", "Four", "Five")
+numvec <- c(1.5, 2.5, 3.5, 4.5, 5.5)
+intvec <- c(1L, 2L, 3L, 4L, 5L)
+facvec

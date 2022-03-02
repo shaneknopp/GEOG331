e174e050157dc997bb9ae868bc6e98428f0ba915
shaneknopp <sknopp@colgate.edu>
2022-03-02 00:13:02 -0500

    works

--- Activity 3.R
+++ Activity 3.R
@@ -62,109 +62,145 @@ print(datW[1,])
 # and the package installs. You really don't want to do this over and over again.
 library(lubridate)
 
-#convert to standardized format
-#date format is m/d/y
+# convert to standardized format
+# date format is m/d/y
 dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
 
-#calculate day of year
+# calculate day of year
 datW$doy <- yday(dates)
-#calculate hour in the day
+# calculate hour in the day
 datW$hour <- hour(dates) + (minute(dates)/60)
-#calculate decimal day of year
+# calculate decimal day of year
 datW$DD <- datW$doy + (datW$hour/24)
-#quick preview of new date calcualtions
+# quick preview of new date calcualtions
 datW[1,]
 
-#see how many values have missing data for each sensor observation
-#air temperature
+# see how many values have missing data for each sensor observation
+# air temperature
 length(which(is.na(datW$air.temperature)))
 
-#wind speed
+# wind speed
 length(which(is.na(datW$wind.speed)))
 
-#precipitation
+# precipitation
 length(which(is.na(datW$precipitation)))
 
-#soil temperature
+# soil temperature
 length(which(is.na(datW$soil.moisture)))
 
-#soil moisture
+# soil moisture
 length(which(is.na(datW$soil.temp)))
 
-#make a plot with filled in points (using pch)
-#line lines
+# make a plot with filled in points (using pch)
 plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
      ylab="Soil moisture (cm3 water per cm3 soil)")
 
-#make a plot with filled in points (using pch)
-#line lines
 plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
      ylab="Air temperature (degrees C)")
 
-#I'm going to make a new column to work with that indicates that I am conducting QAQC
-#because overwriting values should be done cautiously and can lead to confusing issues.
-#It can be particularly confusing when you are just learning R.
-#Here I'm using the ifelse function
-#the first argument is a logical statement to be evaluated as true or false on a vector
-#the second argument is the value that my air.tempQ1 column will be given if the statement
-#is true. The last value is the value that will be given to air.tempQ1 if the statement is false.
-#In this case it is just given the air temperature value
 datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
 
-#check the values at the extreme range of the data
-#and throughout the percentiles
+# check the values at the extreme range of the data
+# and throughout the percentiles
 quantile(datW$air.tempQ1)
 
-#look at days with really low air temperature
+# look at days with really low air temperature
 datW[datW$air.tempQ1 < 8,]  
 
-#look at days with really high air temperature
+# look at days with really high air temperature
 datW[datW$air.tempQ1 > 33,]
 
 
 ##################
 #   Question 5   #
 ##################
-#plot precipitation and lightning strikes on the same plot
-#normalize lighting strikes to match precipitation
+# plot precipitation and lightning strikes on the same plot
+# normalize lighting strikes to match precipitation
 lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
-#make the plot with precipitation and lightning activity marked
-#make it empty to start and add in features
+# make the plot with precipitation and lightning activity marked
+# make it empty to start and add in features
 plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
      type="n")
-#plot precipitation points only when there is precipitation 
-#make the points semi-transparent
+# plot precipitation points only when there is precipitation 
+# make the points semi-transparent
 points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
        col= rgb(95/255,158/255,160/255,.5), pch=15)        
 
-#plot lightning points only when there is lightning     
+# plot lightning points only when there is lightning     
 points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
        col= "tomato3", pch=19)
 
+# Determine if lightscale and datW were modified uniformally by comparing length
+assert(length(c(datW$precipitation)) == length(lightscale), "error: The parameters are not the same size")
 
 ##################
-#   Question 6   #
+#   Question 6   #proof
 ##################
-#filter out storms in wind and air temperature measurements
+# filter out storms in wind and air temperature measurements
 # filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
-#create a new air temp column
+# create a new air temp column
 datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
                           ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
 
+# filter out potential wind speeds due to storm
+# values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm removed
+# create a new wind speed column
+datW$wind.speedQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                            ifelse(datW$precipitation > 5, NA, datW$wind.speed))
+
+# Determine if speedQ2 and tempQ2 were modified uniformally by comparing length
+assert(length(datW$wind.speedQ2) == length(datW$air.tempQ2), "error: The parameters are not the same size")
+
+# Generate a plot for wind speed
+plot(datW$DD , datW$wind.speedQ2, xlab = "Day of Year", ylab = "Wind Speed",
+     type="n")
+
+# plot wind speed points only when there is wind speed 
+# make the points semi-transparent
+points(datW$DD[datW$wind.speedQ2 > 0], datW$wind.speedQ2[datW$wind.speedQ2 > 0],
+       col= "darkgoldenrod2", pch=15) 
+
+# plot line only when there is wind speed
+lines(datW$DD[datW$wind.speedQ2 > 0], datW$wind.speedQ2[datW$wind.speedQ2 > 0],
+      col= "blue", pch=15)
 
 ##################
 #   Question 7   #
 ##################
-#NEED TO DO
+par(mfrow=c(2,2))
+
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+plot(datW$DD, datW$soil.temp, pch = 19, type="b",
+     xlab = "Day of Year", ylab = "Soil temperature (degrees C)")
+
+plot(datW$DD, datW$precipitation, pch = 19, type="b",
+     xlab = "Day of Year", ylab = "Precipitation (mm)")
+
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+
+# save plot to variable (used later)
+plots <- recordPlot()
 
 
 ##################
 #   Question 8   #
 ##################
-#NEED TO DO
+results <- data.frame("Total Precipitation" = round(sum(datW$precipitation, na.rm = TRUE), digits = 3))
+results$avgAirTemp <- mean(datW$air.temperature, na.rm = TRUE)
+results$avgWindSpeed <- round(mean(datW$wind.speed, na.rm = TRUE), digits = 2)
+results$avgSoilMoisture <- round(mean(datW$soil.moisture, na.rm = TRUE), digits =  4)
+results$avgSoilTemp <- round(mean(datW$soil.temp, na.rm = TRUE), digits = 1)
+results$observations <- length(datW$solar.radiation)
+results$timePeriod <- max(datW$DD, na.rm = TRUE)
+# display results table
+results
 
 
 ##################
 #   Question 9   #
 ##################
-#NEED TO DO
+# display plots from variable
+plots

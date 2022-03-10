e656a180b1182b8e8652be3f35d847f8dedd33f9
shaneknopp <sknopp@colgate.edu>
2022-03-10 00:09:40 -0500

    done

--- Activity4.R
+++ Activity4.R
@@ -8,46 +8,65 @@ library(ggplot2)
 #####################################
 ##### Part 1: for loops         #####
 #####################################
+# Subset for Iris versicolor
+flower <- iris[iris$Species == "versicolor",]
+# Vectors for regression variables; index with formula i-1 in loop
+parameters = list(c(flower$Sepal.Length), c(flower$Sepal.Width), #1. iris  sepal length x width
+                  c(flower$Petal.Length), c(flower$Petal.Width), #2. iris  petal length x width
+                  c(flower$Sepal.Length), c(flower$Petal.Length)) #3. iris sepal length x petal length
 
-#Using only data for iris versicolor
-#write a for loop
-#that produces a regression table
-#for each of the following relationships
-#1. iris  sepal length x width
-#2. iris  petal length x width
-#3. iris sepal length x petal length
+# Store regression variables in array
+results = list()
 
-# hint: consider using a list, and also new vectors for regression variables
-
+# Loop for three regressions
+for(i in 1:3) {
+    results[[i]] <- lm(parameters[[((i*2)-1)]]~parameters[[(i*2)]])#parameters 1~2, 3~4, 5~6
+}
+# View results 
+summary(results[[1]])
+summary(results[[2]])
+summary(results[[3]])
 
 
 #####################################
 ##### Part 2: data in dplyr     #####
 #####################################
-
-#use dplyr to join data of maximum height
-#to a new iris data frame
+# Data frame with maximum height of species
 height <- data.frame(Species = c("virginica","setosa","versicolor"),
                      Height.cm = c(60,100,11.8))
 
+# Joined data frame with iris and maximum height
+new_iris <- full_join(iris, height, by = 'Species')
 
 
 #####################################
 ##### Part 3: plots in ggplot2  #####
 #####################################
-
-#look at base R scatter plot
+# Look at base R scatter plot
 plot(iris$Sepal.Length,iris$Sepal.Width)
 
 #3a. now make the same plot in ggplot
+scatter <- ggplot(iris, aes(Sepal.Length, Sepal.Width)) + geom_point()
+# Display plot from variable
+scatter
 
-
-#3b. make a scatter plot with ggplot and get rid of  busy grid lines
-
+#3b. make a scatter plot with ggplot and get rid of busy grid lines
+scatter_no_lines <- scatter + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+# Display plot from variable
+scatter_no_lines
 
 #3c. make a scatter plot with ggplot, remove grid lines, add a title and axis labels, 
 #    show species by color, and make the point size proportional to petal length
+scatter_comprehensive <- ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+            geom_point(aes(color = Species, size = Petal.Length)) + #point size and color
+            # Ref: https://ggplot2.tidyverse.org/reference/scale_manual.html
+            scale_color_manual(values = c("virginica"="darkgoldenrod1", "setosa" = "blue", "versicolor" = "brown3")) +
+            # Ref: https://ggplot2.tidyverse.org/reference/theme.html
+            theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+            # Ref: https://ggplot2.tidyverse.org/reference/labs.html
+            labs(x = "Sepal Length (mm)", y="Sepal Width (mm)", title = "Flower Species by Sepal Length and Width")
 
+scatter_comprehensive
 #####################################
 ##### Question: how did         #####
 ##### arguments differ between  #####

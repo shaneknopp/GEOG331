f1802446351bab09d212731fdd86e97874c62d9c
shaneknopp <sknopp@colgate.edu>
2022-04-15 16:31:51 -0400

    Init commit (loads and organizes data, generates plots)

---
+++ Project.R
@@ -0,0 +1,74 @@
+# Project
+# SK 03/23/22
+
+# Determine operating system
+OS <- .Platform$OS.type
+# Set path for operating system
+if (OS == "unix") {
+  path <- "/Volumes/class/GEOG331_S22/students/sknopp/data/Project/" # MAC file path
+}else if (OS == "windows") {
+  path <- "Z:/GEOG331_S22/students/sknopp/data/Project" # windows file path
+} else {
+  print("ERROR: OS could not be identified")
+}
+setwd(path)
+
+#load in lubridate
+library(lubridate)
+# load the terra package
+library(terra)
+
+#---------- planning to make this in a method & for loop since I will be using data from multiple stationa ------------
+#read in data from Melbourne station
+datM <- read.csv("85612_2010_1_1_2021.csv")
+datM$PRECIPITATION <- ifelse(datM$PRECIPITATION == -99.99, NA, datM$PRECIPITATION)
+datM$MAX.TEMP <- ifelse(datM$MAX.TEMP == -99.9, NA, datM$MAX.TEMP)
+datM$MIN.TEMP <- ifelse(datM$MIN.TEMP == -99.9, NA, datM$MIN.TEMP)
+datM$MEAN.TEMP <- ifelse(datM$MEAN.TEMP == -99.9, NA, datM$MEAN.TEMP)
+
+doys = list()
+count <- 1
+for(i in 1:nrow(datM)){ #Iterate over entire dataframe
+  if(datM$MONTH[i] == 1 && datM$DAY[i] == 1) {
+    count <- 1
+  }
+  doys[[i]] = count
+  count <- count + 1
+}
+datM$doy <- unlist(doys)
+
+#calculate a decimal year, but account for leap year
+datM$decYear <- ifelse(leap_year(datM$YEAR),datM$YEAR + (datM$doy/366),
+                       datM$YEAR + (datM$doy/365))
+
+#----------------------------------------------------------------------
+df2020 <- data.frame(datM$PRECIPITATION[datM$YEAR == 2020], datM$MEAN.TEMP[datM$YEAR == 2020], datM$decYear[datM$YEAR == 2020])
+colnames(df2020) <- c("PRECIPITATION","MEAN.TEMP","decYear")
+
+decday <- ifelse(leap_year(2020),2020 + (252/366),
+                       2020 + (252/365))
+par(mfrow=c(2,2))
+#plot precipitation
+plot(df2020$decYear, df2020$PRECIPITATION, type="l", xlab="Year", ylab="Precipitation (inches)",)
+abline(v=2020.689, col="red")
+
+#plot min temperature
+#plot(datM$decYear, datM$MIN.TEMP, type="l", xlab="Year", ylab="Min Temperature (fahrenheit)",)
+
+#plot max temperature
+#plot(datM$decYear, datM$MAX.TEMP, type="l", xlab="Year", ylab="Max Temperature (fahrenheit)",)
+
+#plot mean temperature
+plot(df2020$decYear, df2020$MEAN.TEMP, type="l", xlab="Year", ylab="Mean Temperature (fahrenheit)",)
+abline(v=2020.689, col="red")
+
+par(mfrow=c(1,1))
+p <- rast("L2020252.L3m_DAY_CYAN_CI_cyano_CYAN_CONUS_300m_7_5.tif")
+plot(p)
+
+
+
+#read in data from Royal Palm Beach station
+datRPB <- read.csv("87760_2010_1_1_2021.csv")
+#read in data from West Palm Beach station
+datRPB <- read.csv("89525_2010_1_1_2021.csv")
